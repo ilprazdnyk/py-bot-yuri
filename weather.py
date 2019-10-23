52f6ec574a3943c5ed4fcf59f50f279c64ff0d76
ilprazdnyk <ilprazdnyk@gmail.com>
2019-10-23 21:43:41 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -6,8 +6,13 @@ def prague_weather():
     observation = owm.weather_at_place('Prague, CZ')
     weather = observation.get_weather()
     temperature = weather.get_temperature('celsius')['temp']
-    weather_text = f'The temperature is {temperature}°'
+    cloud_coverage = weather.get_clouds()
+    weather_status = weather.get_detailed_status()
+    weather_text = f'The weather status is: {weather_status} \nThe temperature is {temperature}° ' \
+                   f'\nThe cloud coverage is {cloud_coverage}%'
     return weather_text
+
+
 
 
 
