b4ad73b19b22b34c798ed3ba68932dcbf4683436
ilprazdnyk <ilprazdnyk@gmail.com>
2019-10-25 19:48:11 +0200

    Update yuri-bot.py

--- yuri-bot.py
+++ yuri-bot.py
@@ -1,3 +1,4 @@
+#importing
 import logging
 import weather
 from texter import echo
@@ -5,27 +6,31 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 
-
+#setting the bot token into container
 updater = Updater(token='842440069:AAGNacGGPSgufJmJgeyCo29sB-zMp5vsp9Q', use_context=True)
-
+#the wrapper for the bot uses logging library which needs setting to special format
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 dispatcher = updater.dispatcher
 
-
+#defenition of the function which is going to be called after someone 
+#starts the dialogue with the bot or types the start command#defenition of the function which is going to be called after someone 
+#starts the dialogue with the bot or types the start command#defenition of the function which is going to be called after someone 
+#starts the dialogue with the bot or types the start command
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-
+#now we have to make a command handler from that function and add it to the dispatcher
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 updater.start_polling()
 
-
+#defenition of the function which is going to be called after someone 
+#types the temp command
 def temp(update, context):
     weather_text = weather.prague_weather()
     context.bot.send_message(chat_id=update.effective_chat.id, text=weather_text)
 
-
+#now just like with the start command we have to make a handler from this one and add it to the dispatcher
 temp_handler = CommandHandler('temp', temp)
 dispatcher.add_handler(temp_handler)
 
