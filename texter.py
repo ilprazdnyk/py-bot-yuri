01097ef141547bc2db8f1bd6de048990fe5279ee
ilprazdnyk <ilprazdnyk@gmail.com>
2019-10-28 22:34:46 +0100

    Update texter.py

--- texter.py
+++ texter.py
@@ -1,3 +1,5 @@
-#Having hard time to extend this function, i will have to read the bot api documentation
-def echo(update, context): #this funcion is going to be executed once bot gets a new message
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+def echo(update, context):
+    sentence = update.message.text
+    result = len(sentence)
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=result)
