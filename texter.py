38bb13f61ff32e42452f3bf57d233480a79ac925
ilprazdnyk <ilprazdnyk@gmail.com>
2019-11-02 15:21:26 +0100

    Update texter.py

--- texter.py
+++ texter.py
@@ -1,4 +1,5 @@
 # a simple callback function for the message handler, which counts number of symbols in a new message
+#the goal is to extend it so it would pass a new update as an input into gpt-2 and send output as a message
 def echo(update, context):
     sentence = update.message.text
     result = len(sentence)
